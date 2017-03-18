9fb112e758d07dc505373329cdf31b7cf1098e87
Aurelia <aurellerua@ymail.com>
2017-03-18 19:38:21 +0700

    first commit

---
+++ src/zootopia/Zootopia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zootopia;
+
+/**
+ *
+ * @author Aurelia
+ */
+public class Zootopia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
